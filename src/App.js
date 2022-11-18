5982328e0cb060ce6484673b6b80af087fd8f974
Mario Vinti <mjvinti@gmail.com>
2022-11-18 15:32:36 -0500

    added router outlet and navigation bar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
+
+const Navigation = () => (
+  <div>
+    <div>
+      <h1>I am the navigation bar</h1>
+    </div>
+    <Outlet />
+  </div>
+);
+
+const Shop = () => <h1>I am the shop page</h1>;
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
