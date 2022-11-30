0b4cb9878acb9e8657d884cef4cae8ed477614e9
Mario Vinti <mjvinti@gmail.com>
2022-11-30 12:02:41 -0500

    created user context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,27 +1,34 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
+
+import { UserContext } from "../../contexts/user.context";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
 import "./navigation.styles.scss";
 
-const Navigation = () => (
-  <Fragment>
-    <div className="navigation">
-      <Link className="logo-container" to="/">
-        <CrwnLogo className="logo" />
-      </Link>
-      <div className="nav-links-container">
-        <Link className="nav-link" to="/shop">
-          SHOP
+const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+  console.log("current user:", currentUser);
+
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
         </Link>
-        <Link className="nav-link" to="/auth">
-          SIGN IN
-        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/auth">
+            SIGN IN
+          </Link>
+        </div>
       </div>
-    </div>
-    <Outlet />
-  </Fragment>
-);
+      <Outlet />
+    </Fragment>
+  );
+};
 
 export default Navigation;
