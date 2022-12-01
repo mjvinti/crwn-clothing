c54d47c37adc46a9fc8f4c401cef316f71b9f016
Mario Vinti <mjvinti@gmail.com>
2022-12-01 11:24:44 -0500

    implemented adding to cart functionality

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,37 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, product) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === product.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === product.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...product, quantity: 1 }];
+};
+
 export const CartContext = createContext({
+  addItemToCart: () => {},
+  cartItems: [],
   isCartOpen: false,
-  setIsCartOpen: () => null,
+  setIsCartOpen: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (product) => {
+    setCartItems(addCartItem(cartItems, product));
+  };
+
+  const value = { addItemToCart, cartItems, isCartOpen, setIsCartOpen };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
