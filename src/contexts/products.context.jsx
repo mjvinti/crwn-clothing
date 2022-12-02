4fda6a4040305c0b8bd91f705e4694db2a3507b9
Mario Vinti <mjvinti@gmail.com>
2022-12-02 12:13:48 -0500

    fetching products and categories

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 
-import { addCollectionAndDocuments } from "../routes/utils/firebase/firebase.utils.js";
+import { getCategoriesAndDocuments } from "../routes/utils/firebase/firebase.utils.js";
 
 export const ProductsContext = createContext({
   products: [],
@@ -8,6 +8,15 @@ export const ProductsContext = createContext({
 
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
+
   const value = { products };
 
   return (
